7c91866e627d526002701651d596924635a85f24
jacjanowski <jjanow7@uic.edu>
2021-06-21 12:49:17 -0500

    Intro to Array Adapters

--- TreeGuysPizza/app/src/main/java/com/jacobjanowski/treeguyspizza/StarterActivity.java
+++ TreeGuysPizza/app/src/main/java/com/jacobjanowski/treeguyspizza/StarterActivity.java
@@ -3,6 +3,7 @@ package com.jacobjanowski.treeguyspizza;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class StarterActivity extends AppCompatActivity {
@@ -13,6 +14,33 @@ public class StarterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_starter);
 
         ListView startersList = findViewById(R.id.list_view_starters);
+
+        String[] dishes = {
+                "Mushroom and tofu maki",
+                "Egg and avocado uramaki",
+                "Melon and lemon soup",
+                "Coconut and chocolate mousse",
+                "Spinach and cabbage wontons",
+                "Broccoli and cucumber soup",
+                "Chilli and aubergine dip",
+                "Chickpea and chilli gyoza",
+                "Sprout and pineapple soup",
+                "Egusi and borscht soup",
+                "Aubergine and egg sushi",
+                "Artichoke and mustard soup",
+                "Peppercorn and tamarind soup",
+                "Parsley and celeriac parcels",
+                "Pasta and broccoli soup",
+                "Potato and courgette soup",
+                "Chickpea and cabbage parcels",
+                "Coriander and peppercorn gyoza",
+                "Pear and chestnut soup",
+                "Pesto and garam masala parcels"
+        };
+
+        ArrayAdapter<String> dishesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dishes);
+
+        startersList.setAdapter(dishesAdapter);
 
 
     }
