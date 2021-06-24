3ff784c10fd2d00c496c0cc99ab0a7bedb6d1671
jacjanowski <jjanow7@uic.edu>
2021-06-24 12:46:11 -0500

    Finish Food app. Created Desserts page.

--- TreeGuysPizza/app/src/main/java/com/jacobjanowski/treeguyspizza/MainActivity.java
+++ TreeGuysPizza/app/src/main/java/com/jacobjanowski/treeguyspizza/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     CardView startersCard;
     CardView mainsCard;
+    CardView dessertsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startersCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_mains);
+        dessertsCard = findViewById(R.id.card_view_desserts);
+
 
         startersCard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,16 +35,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(startersActivityIntent);
 
 
-
             }
         });
 
-        mainsCard.setOnClickListener(new View.OnClickListener(){
+        mainsCard.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 Intent mainCoursesActivityIntent = new Intent(MainActivity.this, MainCoursesActivity.class);
                 startActivity(mainCoursesActivityIntent);
+
+            }
+        });
+        dessertsCard.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent dessertsActivityIntent = new Intent(MainActivity.this, DessertsActivity.class);
+                startActivity(dessertsActivityIntent);
 
             }
         });
