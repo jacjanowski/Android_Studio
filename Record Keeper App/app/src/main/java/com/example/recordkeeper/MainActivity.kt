a0340b88708e45fecf7beb962838cd3b7a923f34
jacjanowski <jjanow7@uic.edu>
2021-08-01 14:47:57 -0500

    Finish Course. Add edit page to running section with dynamic titles using intents.

--- Record Keeper App/app/src/main/java/com/example/recordkeeper/MainActivity.kt
+++ Record Keeper App/app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.recordkeeper
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.fragment.app.commit
 import com.example.recordkeeper.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_different_screen.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener,
+    BottomNavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -19,11 +24,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val theClickListener: View.OnClickListener = MyClickListener()
+
+        binding.bottomNav.setOnNavigationItemSelectedListener(this)
+
+
+    }
+
+    override fun onClick(p0: View?) {
+        Toast.makeText(this, "I have made the Activity a click listener", Toast.LENGTH_LONG)
+    }
+
+    private fun onRunningClicked() {
         supportFragmentManager.commit {
-
+            replace(R.id.frame_content, RunningFragment())
         }
+    }
 
+    private fun onCyclingClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, BikingFragment())
+        }
+    }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.nav_biking) {
+            onCyclingClicked()
+            return true
+        } else if (item.itemId == R.id.nav_running) {
+            onRunningClicked()
+            return true
+        } else {
+            return false
+        }
     }
 }
 
---
+++ Record Keeper App/app/src/main/java/com/example/recordkeeper/MyClickListener.kt
@@ -0,0 +1,11 @@
+package com.example.recordkeeper
+
+import android.view.View
+
+class MyClickListener: View.OnClickListener {
+    override fun onClick(p0: View?) {
+
+    }
+
+
+}
